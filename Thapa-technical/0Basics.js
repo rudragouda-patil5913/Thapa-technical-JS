e126215d426057392e8533e866a007d764f126bf
Rudragouda Patil <88940283+rudragouda-patil5913@users.noreply.github.com>
2023-05-22 15:21:21 +0530

    Add files via upload

---
+++ Thapa-technical/0Basics.js
@@ -0,0 +1,49 @@
+// types of Data
+
+// console.log(typeof a);
+// console.log(typeof true);
+// console.log(typeof 9);
+// console.log(typeof "rudra");
+// console.log(typeof 1n);
+// console.log(typeof null);
+// console.log(typeof Symbol());
+
+// console.log("ram"+"hari")
+// console.log("ram"-"hari")
+// console.log("10" + 20)
+// console.log("10" - 20);
+
+// console.log(true + true);
+// console.log(true - true);
+// console.log(true + false);
+// console.log(true - false);
+// console.log(false - false);
+// console.log(false + false);
+// console.log(false - true);
+
+//null vs undefined
+
+// var a = null;
+// console.log(a);
+// a = 10;
+// console.log(a);
+// var stand;
+// console.log(stand);
+// console.log(b); // not defined
+
+//NaN
+// var myNam = "Rud";
+// if(isNaN(myNam)){
+//     console.log("enter the number");
+// }
+// var myNum = "8";
+// console.log(typeof isNaN(myNum));
+// console.log(isNaN(myNum));
+// if (isNaN(myNum)) {
+//   console.log("enter the number");
+// } else {
+//   console.log("It's number");
+// }
+
+
+ 
